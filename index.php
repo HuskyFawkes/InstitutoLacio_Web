cb1f707622e9fa7b6ce7d452d821fa7863ed9dcd
xGustavox <gustavo.camargos99@gmail.com>
2018-02-17 23:01:39 -0200

    Commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="dist/css/bootstrap.css">
+    <link rel="stylesheet" href="dist/css/index.css">
+  </head>
+  <body>
+    <header>
+
+    </header>
+    <section>
+
+    </section>
+    <section>
+
+    </section>
+    <section>
+
+    </section>
+    <section>
+
+    </section>
+    <section>
+
+    </section>
+    <script src="dist/js/bootstrap.min.js" charset="utf-8"></script>
+  </body>
+</html>
