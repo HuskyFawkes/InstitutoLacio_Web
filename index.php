fad447bfef74ba00d344d44b863cd9ab0cbf9238
xGustavox <gustavo.camargos99@gmail.com>
2018-02-18 01:03:17 -0300

    aaaaaaaa

--- index.php
+++ index.php
@@ -134,8 +134,95 @@
           </div>
         </div>
       </section>
-      <section>
-
+      <section class="sessao123">
+        <h1>CONTEÚDO DO CURSO</h1>
+        <div class="textao">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                <p>1 Introduçaõ à poesia em lingua inglesae à metrica
+                Robert Frost: Stopping by woods on a Snowy Evening
+                Joyce Kilmer: trees
+                Gramática: formas possessivas, subjetivas e objetivas dos pronomes</p>
+                <p>2. Dicçao, Smiles e Metáforas: uma introdução às figuras.
+                Shakespeare: Witches'incantation, Macbeth(act 4, Scene !)
+                Sir John Harrington:Treason
+                Gramática:o pronome de segunda pessoa, terceira pessoa arcaica,
+                imperativo e 'do' enfático.</p>
+                <p>3.  A poesia e a linguagem cotidiana:neutralidade, suspension of disbelief.
+                Analogia, alegoria e paradoxo
+                Robert Frost:Neither out far nor in deep.Kafka: a little fable
+                Gramática:"as long as", simple past, present perfect</p>
+                <p>4.  Poesia e mentiras: ambiguidades, analogia, anáfora e alegoria
+                Robert Frost, The Road not taken, the dust of snow.
+                Gramática:present future with shall, 'as...as', as far as...,
+                should.</p>
+                <p>5.  Metáfora, poesia e educação
+                Robert Frost, Education by poetry (excerto).Dylan Thomas, The
+                hand the singed the paper,
+                Gramática:future with will, would, voz passive, superlative</p>
+                <p>6  Poesia e transcendência
+                Robert Frost, Birches
+                Gramática:orações condicionais</p>
+                <p>8.  Formas livres e liberdade:verso livre métrica e expressão
+                Excerto de Robert Frost fictional lecture in Tobias Wolfe. Willian
+                Wordsworth,Nuns not free
+                Gramática:orações concesivas, pronome "one", adjetivo "no".
+                sentenças negativas.</p>
+                <p>9.  Poesia e luto:sonetos italianos e figuras de repetição
+                Willina Wordsworth, Surprised by joy. Composed upon Westminster
+                Bridge
+                Gramática:"Neither...nor", verbos modais</p>
+                <p>10  Poesia e mito: aliteração, aenjambement, dísticos
+                W.B Yeats, Leda and the Swan. Robert Frost, into my own
+                Gramática:orações adverbiais, verbos modais</p>
+              </div>
+              <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                <p>11. Poesia e mortalidade:sonetos shakespeareanos, praeterino,
+                silepse e acumulação
+                Shakespeare, Soneto 12, Soneto 18
+                Gramática: comparação,"when...then"</p>
+                <p>12  Poesia e filosofia: símbolo, alteraçaõ, hiberbato, anthimeria,
+                apóstrophe
+                G.M Hopkins, As Kingfishers catch fire, Joseph Blanco-White, Night
+                and Death</p>
+                <p>Gramática:as partes do discurso, a flexibilidade das palavras em inglês.
+                13.  Poesia e imaginação: verso branco, metáfora e smile
+                Shakespeare, Prólogo a Henry V
+                Gramática: verbos modais</p>
+                <p>14.  Poesia e ação: a linguagem shakespearana
+                Shakespeare, Richard II, Act 3, Scene 2, 2150-182
+                Gramática: orações acumulativas e adversativas, gerúndio e subjuntivo</p>
+                <p>15.  Poesia e o conhecimento: disticos heroicos
+                Alexander Pope, Essay on Man, 1-18; 215-218
+                Gramática:orações alternativas</p>
+                <p>16.  Poesia e solidão
+                Willian Wordsworth, I wandered lonely as a clound
+                Gramática: nominative absolute, gerúndio</p>
+                <p>17.  Poesia e empatia: onomatopeia e enhambement
+                John Fandel, The bee. Shakespeare, Richard II, act 3 Scene IV 32 - 72
+                Gramática:past perfect, present perfect, past participles</p>
+                <p>18.  Poesia e profecia: exempla e polyptoton
+                Shakespeare, Richard II, act 2, Scene I, 32-74
+                Gramática: present perfect "for" para expressar interesse, "wont to",
+                "used to"</p>
+                <p>19.  Poesia e modernidades: terza rima e inclusão
+                John Keats, Four Seasons fill The measure of the year. Rober Frost.
+                Acquainted with the night, Never again would bird's song the same.</p>
+                <p>20.  Poesia e imitação: conclusão do curso
+                Ovidio, The myth of Narcissus. Pedro Cava, Off the Glass</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="">
+          <div class="texto comprar-alt">
+            De <span class="ruim">R$800,00</span> por <br>R$500,00 à vista ou <br><span class="bom">10xR$60,00</span>
+          </div>
+          <div class="botao-comprar-topo">
+            <button type="button" name="button" class="botao-comprar">COMPRAR</button>
+          </div>
+        </div>
       </section>
       <section class="ultima-sessao">
         <div class="imagem">
